4997174083127b98f08dafefbe0b1bc9ced8d4ea
Claire Delosa <clair650@gmail.com>
2023-05-30 20:23:45 +1000

    First commit. Pushing new files and readme.

---
+++ session_1/variables.py
@@ -0,0 +1,58 @@
+# Create a variable named first_name and assign Claire to it
+first_name = "Claire"
+last_name = "Del"
+programming_language = "python"
+
+# Data Types - Text data, numerical data
+# Text data - String
+
+today = "Tuesday"
+# print(today)
+# print(f"Today is {today}")
+# print(type(today))
+
+# Numerical data types - Integer, Float
+# Integer - whole number
+parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+height = 170
+# print(f"My height is {height/100} in m.")
+
+#float
+weight = 68.2
+# print(type(weight))
+# print(f"My weight is {weight*2.1} in lbs.")
+
+distance = "5000"
+# print(int(distance) + 8)
+# print(distance + str(8))
+
+# print("Hello")
+# dog_name = input("What is the dogs name?")
+# print(f"Nice to meet you {dog_name}")
+
+# birth_year = input("What year were you born?")
+# print(f"You are {2023-int(birth_year)} years old.")
+
+# Q1: Write a program that takes two numbers from the user,and outputs their sum.
+# first_number = input("Choose a number:")
+# second_number = input("Choose another number:")
+# print(f"The sum of your numbers is {int(first_number)+ int(second_number)}")
+
+# Q2: Write a program that takes two numbers from the user,and outputs the equation representing the multiplication ofthe two numbers.
+# first_number = input("Choose a number:")
+# second_number = input("Choose another number:")
+# print(f"The sum of your numbers is {int(first_number)* int(second_number)}")
+
+# Q3: Write a program that takes a distance in kilometers fromthe user, and output the distance in meters and centimeters.
+# kilometers = input("How many kilometers?")
+# print(f"kilometers = {int(kilometers)*100} in m.")
+# print(f"kilometers = {int(kilometers)*1000} in cm.")
+
+# Q4: Write a program that takes the user's name and height(in centimeters), and outputs a summary sentence.
+# user_name = input("What is your name?")
+# user_height = input("What is your height in cm?")
+# print(f"Hello {user_name}, you are {int(user_height)/100}m tall!")
+
+#Q5: Is the input function a statement or an expression?
